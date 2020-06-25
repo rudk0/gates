e2a3d5a3b4f6354cbc0f9e422f0a6fd4babf8d2b
Agatha Rudko <agatharudko@gmail.com>
2020-06-25 18:11:05 +0300

    init

---
+++ precache-manifest.69166d7d46098e3432fee88b1af6d5c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26d86366830db4e06df347d63bca607d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b6fd6223a84ad6f7403",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "9d471e17b91e70544747",
+    "url": "/static/css/main.4eeb3af2.chunk.css"
+  },
+  {
+    "revision": "0b6fd6223a84ad6f7403",
+    "url": "/static/js/2.10d278c1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.10d278c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d471e17b91e70544747",
+    "url": "/static/js/main.a16c7455.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
